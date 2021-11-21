04da786177b7a8d33e0bfb9565eb537b907effe9
nvsang260720 <nvsang.18it2@vku.udn.vn>
2021-11-21 23:05:03 +0700

    edit

--- main.R
+++ main.R
@@ -36,7 +36,7 @@ df[["total_Critic"]] = df[["Critic_Score"]] + df[["User_Score"]]
 df["Rating"] = NULL
 
 
-ui <- dashboardPage(skin = "black",
+ui = dashboardPage(skin = "black",
   
   dashboardHeader(
     title = "Game Statistics"
@@ -68,6 +68,18 @@ ui <- dashboardPage(skin = "black",
             .h2-title {
               color:red;
             }
+            .box.box-solid.box-primary>.box-header {
+                    color:#fff;
+                    background:#666666
+                }
+
+            .box.box-solid.box-primary{
+                  border-bottom-color:#666666;
+                  border-left-color:#666666;
+                  border-right-color:#666666;
+                  border-top-color:#666666;
+            }
+            
             
           ")
       )
@@ -77,25 +89,24 @@ ui <- dashboardPage(skin = "black",
       # Second tab content
       tabItem(tabName = "Dataset",
               fluidRow(
-                column(12,
-                       box(
-                         title = "Show data in table",
-                         status = "info",
-                         solidHeader = TRUE, 
-                         collapsible = TRUE,
-                         style = (" color: White "),
-                         div( 
-                            div( 
-                                 p("Filter information from data."),
-                                 p("We can search the game name through the information filter and the search bar.",
-                                   style = (""))
-                            )
-                         )
-                       )   
+                 box(
+                   
+                   with =4,
+                   title = "Show data in table",
+                   status = "info",
+                   solidHeader = TRUE, 
+                   collapsible = TRUE,
+                   div( 
+                      div( 
+                           p("Filter information from data."),
+                           p("We can search the game name through the information filter and the search bar.",
+                             )
+                      )
+                   )
                 )
               ),#end fluidRow
               fluidRow(
-                box( width = ("null"),
+                box( width = 12,
                 
                 column(4,
                        selectInput("Platform",
@@ -128,54 +139,94 @@ ui <- dashboardPage(skin = "black",
       # First tab content
       tabItem(tabName = "Topgame",
               fluidRow(
-                column(12,
-                       box(
-                          title = "Top Sales in the world",
-                          status = "info",
-                          solidHeader = TRUE, 
-                          collapsible = TRUE,
-                          width = ("null"),
-                          div( 
-                            selectInput("Years",
-                                        label = "Choose Sales Year",
-                                        c("All",
-                                          sort(decreasing = TRUE,unique(as.character(df$Year_of_Release)))
-                                        )
-                                      ),
-                            plotOutput("plot",
-                                        width = "100%",
-                                        height = "400px",
-                                        click = "plot_click",
-                                        dblclick = NULL,
-                                        hover = hoverOpts(id = "plot_hover", delayType = "throttle"),
-                                        brush = brushOpts(id = "plot_brush"),
-                                        inline = FALSE
-                                        
-                                      )
-                              )#end div
-                           )#end box
-                       )# end colum
+                box(width = 3,
+                    class = (""),
+                    title = "Number Games",
+                    status = "success", 
+                    solidHeader = TRUE,
+                    div(
+                      span(textOutput("text_name"))
+                      
+                    )
+                ),
+                box(width = 3,
+                    class = (""),
+                    title = "Number Platform",
+                    status = "info", 
+                    solidHeader = TRUE,
+                    div(
+                      span(textOutput("text_platform"))
+                      
+                    )
+                ),
+                box(width = 3,
+                    class = (""),
+                    title = "Number Genre",
+                    status = "warning", 
+                    solidHeader = TRUE,
+                    div(
+                      span(textOutput("text_genre"))
+                      
+                    )
+                ),
+                box(width = 3,
+                    class = (""),
+                    title = "Number Publisher",
+                    status = "danger", 
+                    solidHeader = TRUE,
+                    div(
+                      span(textOutput("text_publisher"))
+                      
+                    )
+                )
+              ),
+              fluidRow(
+                 box(
+                    title = "Top Sales in the world",
+                    status = "info",
+                    solidHeader = TRUE, 
+                    collapsible = TRUE,
+                    width = 12,
+                    div( 
+                      selectInput("Years",
+                                  label = "Choose Sales Year",
+                                  c("All",
+                                    sort(decreasing = TRUE,unique(as.character(df$Year_of_Release)))
+                                  )
+                                ),
+                      plotOutput("plot",
+                                  width = "100%",
+                                  height = "400px",
+                                  click = "plot_click",
+                                  dblclick = NULL,
+                                  hover = hoverOpts(id = "plot_hover", delayType = "throttle"),
+                                  brush = brushOpts(id = "plot_brush"),
+                                  inline = FALSE
+                                  
+                                )
+                        )#end div
+                     )#end box
                 ), #end fluidRow
                 fluidRow(
-                  column(12,
-                         box(
-                           title = "Top Genre in the world",
-                           status = "info",
-                           solidHeader = TRUE, 
-                           collapsible = TRUE,
-                           div(
-                              plotOutput("Plot_1",
-                                         width = "100%",
-                                         height = "400px",
-                                         click = "plot_click",
-                                         dblclick = NULL,
-                                         hover = hoverOpts(id = "plot_hover", delayType = "throttle"),
-                                         brush = brushOpts(id = "plot_brush"),
-                                         inline = FALSE
-                                         )
-                           )#end div
-                         )#end box
-                      )#end colum
+                   box(
+                     title = "Top Genre in the world",
+                     status = "info",
+                     solidHeader = TRUE, 
+                     collapsible = TRUE,
+                     column(12,
+                     div(
+                        
+                        plotOutput("Plot_1",
+                                   width = "100%",
+                                   height = "400px",
+                                   click = "plot_click",
+                                   dblclick = NULL,
+                                   inline = FALSE
+                                   ),
+                      
+                     )#end div
+                   )#end column
+                )#end box
                 ) # end fluidRow
       ) #end tab item 
       
@@ -183,44 +234,83 @@ ui <- dashboardPage(skin = "black",
   )#end dashboardPage
 )#end ui
 
-server <- function(input, output, session) {
-  output$Plot_1 <- renderPlot({
-    df2 <- df %>% group_by(Genre) %>%  summarise(Sales=sum(total_Critic))
-    
+server = function(input, output, session) {
+  group_name = df %>% group_by(Name) %>% summarise(Sales=sum(as.numeric(total_Critic)))
+  group_platform = df %>% group_by(Platform)
+  group_genre = df %>% group_by(Genre)
+  group_publisher = df %>% group_by(Publisher)
+  
+  select_name= c(unique(as.character(group_name$Name)))
+  select_platform= c(unique(as.character(group_platform$Platform)))
+  select_genre= c(unique(as.character(group_genre$Genre)))
+  select_publisher= c(unique(as.character(group_publisher$Publisher)))
+  
+  leng_name = round(as.numeric(length(select_name)))
+  leng_platform = round(as.numeric(length(select_platform)))
+  leng_genre = round(as.numeric(length(select_genre)))
+  leng_publisher = round(as.numeric(length(select_publisher)))
+  
+
+  data2 = na.omit(group_name)
+  #df6 = data2[order(data2$Sales, decreasing = TRUE)]
+  
+  print(data2)
+
+  output$text_name = renderText(
+    { 
+      leng_name
+    }
+  )
+  output$text_platform = renderText(
+    { 
+      leng_platform
+    }
+  )
+  output$text_genre = renderText(
+    { 
+      leng_genre
+    }
+  )
+  output$text_publisher = renderText(
+    { 
+      leng_publisher
+    }
+  )
+  
+  output$Plot_1 = renderPlot({
+    df2 = df %>% group_by(Genre) %>%  summarise(Sales=sum(total_Critic))
     df3 = c(unique(as.character(df2$Sales)))
     title= c(unique(as.character(df2$Genre)))
-    pct <- round(as.numeric(df3)/sum(as.numeric(df3))*100)
+    pct = round(as.numeric(df3)/sum(as.numeric(df3))*100)
 
-    lbls <- paste(pct) # add percents to labels
-    lbls <- paste(lbls,"%",sep="") # ad % to labels
+    lbls = paste(pct) # add percents to labels
+    lbls = paste(lbls,"%",sep="") # ad % to labels
     # Render a barplot
     pie(pct, 
         main="Years (1976-2017)",
         col=rainbow(12),
         labels = lbls)
-    legend("topright", title , cex=0.8,fill=rainbow(length(pct)))
+    legend("topright", title , cex=1,fill=rainbow(length(pct)))
   })
-
-  output$nthValue <- renderText({ "hello" })
-  output$plot <- renderPlot({
+  output$plot = renderPlot({
     if (input$Years != "All") {
-      data <-reactive({
+      data = reactive({
         df %>%filter(Year_of_Release %in% input$Years) %>% group_by(Platform) %>%  summarise(Sales=sum(Global_Sales))
       })
     }else{
-      data <-reactive({
+      data = reactive({
         df %>% group_by(Platform) %>%  summarise(Sales=sum(Global_Sales))
       })
     }
 
-    color <- c("blue", "red")
+    color = c("blue", "red")
     
-    g <- ggplot(data(), aes( y = Sales, x = Platform, fill = "Global Sales"), col=color)
+    g = ggplot(data(), aes( y = Sales, x = Platform, fill = "Global Sales"), col=color)
     g + geom_bar(stat = "identity")
   })
   
   # Filter data based on selections
-  output$table <- renderDT(DT::datatable(
+  output$table =  renderDT(DT::datatable(
     options = list(
       className = 'dt-center',
       pageLength = 5,
@@ -232,15 +322,15 @@ server <- function(input, output, session) {
     selection = 'none',
     class = 'cell-border strip hover',
     {
-      data <- df
+      data = df
       if (input$Platform != "All") {
-        data <- data[data$Platform == input$Platform,]
+        data = data[data$Platform == input$Platform,]
       }
       if (input$Year_of_Release != "All") {
-        data <- data[data$Year_of_Release == input$Year_of_Release,]
+        data = data[data$Year_of_Release == input$Year_of_Release,]
       }
       if (input$Genre != "All") {
-        data <- data[data$Genre == input$Genre,]
+        data = data[data$Genre == input$Genre,]
       }
       
       data
